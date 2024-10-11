e6bbc7accd62b16edfe8e40ec5978acdf065e476
furkan <furkantaglik2334@gmail.com>
2024-10-12 00:11:13 +0300

    about,skills page added

--- src/components/general/Arrow.tsx
+++ src/components/general/Arrow.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Arrow({ margin = 40 }) {
   return (
-    <div className={`flex justify-center items-center my-${margin}`}>
+    <div className={`flex justify-center items-center my-40`}>
       <ChevronsDown className=" animate-bounce" color="cyan" size={30} />
     </div>
   );
