e6075d95494821bab58eee0577ca0356b123d65f
furkan <furkantaglik2334@gmail.com>
2024-10-21 01:07:31 +0300

    cv deleted

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const projectsTable = pgTable("projects", {
   id: serial("id").primaryKey(),
@@ -12,4 +12,20 @@ export const projectsTable = pgTable("projects", {
 
 export const certificatesTable = pgTable("certificates", {
   id: serial("id").primaryKey(),
+  title: varchar("title", { length: 200 }).notNull(),
+  company: varchar("company", { length: 100 }).notNull(),
+  source: varchar("source", { length: 100 }).notNull(),
+});
+
+export const educationsTable = pgTable("educations", {
+  id: serial("id").primaryKey(),
+  school: varchar("school", { length: 200 }).notNull(),
+  section: varchar("section", { length: 200 }).notNull(),
+  year: varchar("year", { length: 100 }).notNull(),
+});
+
+export const experiencestable = pgTable("experience", {
+  id: serial("id").primaryKey(),
+  company: varchar("company", { length: 200 }).notNull(),
+  role: varchar("role", { length: 100 }).notNull(),
 });
